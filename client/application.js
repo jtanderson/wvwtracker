ebe2915d8214f0083395f6c28a80ca7f6cc36bd0
Joseph Anderson <jtanderson@ratiocaeli.com>
2015-08-19 16:37:03 -0400

    Add some zoom controls

--- client/application.js
+++ client/application.js
@@ -18,17 +18,22 @@ Template.allAreas.onRendered(
 		}
 
 		var minzoom = 3;
+		var Leaflet = L;
 
 		map = L.map("map", {
 			minZoom: minzoom,
 			maxZoom: 7,
 			crs: L.CRS.Simple,
 			dragging: true,
-			zoomControl: false,
-			bounceAtZoomLimits: false
+			zoomControl: true,
+			bounceAtZoomLimits: false,
+			scrollWheelZoom: false
 		}).setView([0, 0], 0);
 
+		map["view"] = "world";
+
 		map.on("click", function(e){
+			console.log("You clicked the map at point: " + e.latlng);
 		});
 
 		var southWest = [-250, 75];
@@ -111,6 +116,131 @@ Template.allAreas.onRendered(
 				Tracker.autorun(countTrackerFn);
 			}
 		};
+
+		var ebBounds = [
+			[-245.625,138.75],
+			[-195,186.75]
+		];
+		var greenHomeBounds = [
+			[-234,87],
+			[-181,129]
+		];
+		var redHomeBounds = [
+			[-195,144],
+			[-140,186]
+		];
+		var blueHomeBounds = [
+			[-223,199],
+			[-171,243]
+		];
+		var ebMarkerLoc = [-202,164];
+		var greenMarkerLoc = [-178,103.5];
+		var redMarkerLoc = [-137,161];
+		var blueMarkerLoc = [-168,217];
+
+		var ebMarker = L.marker(ebMarkerLoc, {
+			icon: new L.divIcon({
+				html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span> EB</button>',
+				iconSize: [32,32],
+				className: "mapbutton"
+			})
+		}).on('click', function(event){
+			if ( map.view == "world" ){
+				map.fitBounds(ebBounds);
+				this.setIcon(new L.divIcon({
+					html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-minus"></span> EB</button>',
+					iconSize: [32,32],
+					className: "mapbutton"
+				}));
+				map.view = "eb";
+			} else {
+				map.fitWorld();
+				map.view = "world";
+				this.setIcon(new L.divIcon({
+					html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span> EB</button>',
+					iconSize: [32,32],
+					className: "mapbutton"
+				}));
+			}
+		}).addTo(map);
+
+		var greenHomeMarker = L.marker(greenMarkerLoc, {
+			icon: new L.divIcon({
+				html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span> Green Home</button>',
+				iconSize: [32,32],
+				className: "mapbutton"
+			})
+		}).on('click', function(event){
+			if ( map.view == "world" ){
+				map.fitBounds(greenHomeBounds);
+				this.setIcon(new L.divIcon({
+					html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-minus"></span> Green Home</button>',
+					iconSize: [32,32],
+					className: "mapbutton"
+				}));
+				map.view = "green";
+			} else {
+				map.fitWorld();
+				map.view = "world";
+				this.setIcon(new L.divIcon({
+					html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span> Green Home</button>',
+					iconSize: [32,32],
+					className: "mapbutton"
+				}));
+			}
+		}).addTo(map);
+
+		var redHomeMarker = L.marker(redMarkerLoc, {
+			icon: new L.divIcon({
+				html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span> Red Home</button>',
+				iconSize: [32,32],
+				className: "mapbutton"
+			})
+		}).on('click', function(event){
+			if ( map.view == "world" ){
+				map.fitBounds(redHomeBounds);
+				this.setIcon(new L.divIcon({
+					html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-minus"></span> Red Home</button>',
+					iconSize: [32,32],
+					className: "mapbutton"
+				}));
+				map.view = "red";
+			} else {
+				map.fitWorld();
+				map.view = "world";
+				this.setIcon(new L.divIcon({
+					html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span> Red Home</button>',
+					iconSize: [32,32],
+					className: "mapbutton"
+				}));
+			}
+		}).addTo(map);
+
+		var blueHomeMarker = L.marker(blueMarkerLoc, {
+			icon: new L.divIcon({
+				html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span> Blue Home</button>',
+				iconSize: [32,32],
+				className: "mapbutton"
+			})
+		}).on('click', function(event){
+			if ( map.view == "world" ){
+				map.fitBounds(blueHomeBounds);
+				this.setIcon(new L.divIcon({
+					html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-minus"></span> Blue Home</button>',
+					iconSize: [32,32],
+					className: "mapbutton"
+				}));
+				map.view = "blue";
+			} else {
+				map.fitWorld();
+				map.view = "world";
+				this.setIcon(new L.divIcon({
+					html: '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus"></span> Blue Home</button>',
+					iconSize: [32,32],
+					className: "mapbutton"
+				}));
+			}
+		}).addTo(map);
 	}
 );
 
